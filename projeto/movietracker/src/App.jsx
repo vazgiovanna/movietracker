25665bc4d25b4c877715980fd85a8ebcb6da445d
Arvicx <arthurfernandesrodrigues1@gmail.com>
2025-08-05 17:42:27 -0300

    05/08
    Integração incompleta da API + Documentação

--- projeto/movietracker/src/App.jsx
+++ projeto/movietracker/src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
+import axios from 'axios';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+async function API(){
+  try {
+      const resposta = await axios.get(
+        'https://api.themoviedb.org/3',
+      );
+      return resposta.data;
+    } catch (error) {
+      console.log(error);
+    }
+}
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
- 
+      <API/>
     </>
   )
 }
