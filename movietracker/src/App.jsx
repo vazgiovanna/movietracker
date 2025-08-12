9375115457b395163d5cc7ff11de3125ebf6cb2b
vazgiovanna <giovannaaguiarvaz@gmail.com>
2025-08-12 17:35:25 -0300

    Api configurada

--- movietracker/src/App.jsx
+++ movietracker/src/App.jsx
@@ -1,26 +1,15 @@
-import { useState } from 'react'
 import axios from 'axios';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-async function API(){
+function App() {
   try {
-      const resposta = await axios.get(
+      const resposta = axios.get(
         'https://api.themoviedb.org/3',
       );
       return resposta.data;
     } catch (error) {
       console.log(error);
     }
-}
-function App() {
-  const [count, setCount] = useState(0)
-  return (
-    <>
-      <API/>
-    </>
-  )
 }
 
 export default App
--- movietracker/src/main.jsx
+++ movietracker/src/main.jsx
@@ -3,17 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Home from './components/Home/Home.jsx'
-import Pagina2 from './components/Pagina2/Pagina2.jsx'
+import Busca from './components/Busca.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-{/* header */}
 
-{/* rota - if rota home = <Home /> */}
-
-{/* footer */}
-
-    <App />
-    <Pagina2/>
+    <App/>
+    <Home/>
+    <Busca/>
   </StrictMode>,
 )
