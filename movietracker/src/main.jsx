189afd9631116bafe19b5c3fd6485af39ab22df9
vazgiovanna <giovannaaguiarvaz@gmail.com>
2025-08-12 14:50:57 -0300

    sem app

--- movietracker/src/main.jsx
+++ movietracker/src/main.jsx
@@ -7,6 +7,12 @@ import Pagina2 from './components/Pagina2/Pagina2.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+{/* header */}
+
+{/* rota - if rota home = <Home /> */}
+
+{/* footer */}
+
     <App />
     <Pagina2/>
   </StrictMode>,
