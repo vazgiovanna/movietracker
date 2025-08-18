720a2769a306179950059f6c20afce1864a285f4
Arvicx <arthurfernandesrodrigues1@gmail.com>
2025-08-18 15:04:36 -0300

    Imagens para o Catálogo

--- movietracker/src/components/Home/Home.jsx
+++ movietracker/src/components/Home/Home.jsx
@@ -6,19 +6,23 @@ function Home(){
         <div className="catalogo">
             <div className="cards">
                 <div className='card bg-gray-950'>
+                    <img src="src\components\images\HappyFeet.jpg" alt="" />
+                </div>
 
-                </div>
                 <div className='card bg-gray-950'>
-                    
+                    <img src="src\components\images\Lilo&Stitch.jpg" alt="" />
                 </div>
+
                 <div className='card bg-gray-950'>
-                    
+                    <img src="src\components\images\Superman.jpg" alt="" />
                 </div>
+
                 <div className='card bg-gray-950'>
-                    
+                    <img src="src\components\images\TaDandoOnda.jpg" alt="" />
                 </div>
+
                 <div className='card bg-gray-950'>
-                    
+                    <img src="src\components\images\TheBatman.jpg" alt="" />
                 </div>
                 
             </div>
--- movietracker/src/main.jsx
+++ movietracker/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
 import Home from './components/Home/Home.jsx'
-import Pagina2 from './components/Pagina2/Pagina2.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
